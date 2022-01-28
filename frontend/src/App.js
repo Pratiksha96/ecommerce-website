cca4130c4e97c25b478b62265163b31976a023a1
Aashish <aashish.d.220@gmail.com>
2022-01-28 18:39:26 -0500

    Resolved #14 by creating header and footer (#17)
    * Header added
    
    * Footer Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './component/layout/Header/Header.js'
+import Footer from './component/layout/Footer/Footer.js'
 
-function App() {
+
+import {BrowserRouter as Router} from 'react-router-dom'
+import WebFont from 'webfontloader';
+
+
+
+const App = () => {
+  React.useEffect(()=>{
+    WebFont.load({
+      google:{
+        families:["Roboto", "Droid Sans","Chilanka"]
+      }
+    })
+    },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+        <Header/> 
+        <Footer/>
+    </Router>
+  )
 }
 
-export default App;
+export default App
