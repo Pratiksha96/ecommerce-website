12c214ec602461d7d8a69dc1696bff923b35fdc7
Aashish Dhalla <aashish.d.220@gmail.com>
2022-01-28 18:36:50 -0500

    Adding Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from './component/layout/Header/Header.js'
 import Footer from './component/layout/Footer/Footer.js'
+import Home from './component/Home/Home.js'
 
 
-import {BrowserRouter as Router} from 'react-router-dom'
+
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 import WebFont from 'webfontloader';
 
 
@@ -19,6 +21,7 @@ const App = () => {
   return (
     <Router>
         <Header/> 
+        <Route exact path="/" component={Home}/>
         <Footer/>
     </Router>
   )
