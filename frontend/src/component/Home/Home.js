57d4c197723749f63a756df19d2ad5db95d25b94
Aashish Dhalla <aashish.d.220@gmail.com>
2022-02-04 14:23:57 -0500

    Products Page Added

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,11 +1,24 @@
 import React, { Fragment } from 'react'
+import Product from './Product.js'
 // import {CgMouse} from 'react-icons'
+import axios from 'axios'
 import './Home.css'
+import Metadata from '../layout/MetaData'
 const Home = () => {
+
+    const data=axios.post('https://localhost:8080/addProduct')
+    const product ={
+        name:'Blue Tshirt',
+        images:[{url:"https://i.ibb.co/DRST11n/1.webp"}],
+        price:'$400',
+        _id:'aashish'
+
+    }
     return (
         <Fragment>
+            <Metadata title="Ecommerce"/>
          <div className="banner">
-        <p> Welcome to Ecommerce</p>
+        <p> Welcome to the Future</p>
         <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
         <a href="#container">
@@ -15,6 +28,25 @@ const Home = () => {
             </button>
         </a>
          </div>
+        <h2 className="homeHeading">
+            Featured Products
+        </h2>
+        <div className="container" id="container">
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+            <Product product={product}/>
+
+
+        </div>
+
         </Fragment>
     )
 }
