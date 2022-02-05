beee6b0e0caa19af92c487051e06483c38228f5a
Pranav Seth <pranavseth26@gmail.com>
2022-02-04 19:35:58 -0500

    Resolved Issue #32 by creating and implementing interface

--- backend/app/middleware/apiImplementation.go
+++ backend/app/middleware/apiImplementation.go
@@ -1,11 +1,13 @@
 package middleware
 
 import (
-	//"context"
 	"context"
 	models "ecommerce-website/app/Models"
 	"ecommerce-website/app/utils"
 	"ecommerce-website/internal/database"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"encoding/json"
 	"fmt"
@@ -24,5 +26,49 @@ func CreateProduct(product models.Product, w http.ResponseWriter) {
 	fmt.Println("Product Inserted", insertedProduct.InsertedID)
 
 	json.NewEncoder(w).Encode(product)
+
+}
+func GetProduct(id primitive.ObjectID, w http.ResponseWriter) {
+
+	var product models.Product
+	filter := bson.M{"_id": id}
+	err := database.Coll_product.FindOne(context.TODO(), filter).Decode(&product)
+
+	if err != nil {
+		utils.GetError(err, w)
+		return
+	}
+
+	json.NewEncoder(w).Encode(product)
+
+}
+
+func GetAllProducts(w http.ResponseWriter) {
+	cur, err := database.Coll_product.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		utils.GetError(err, w)
+		return
+	}
+
+	var results []primitive.M
+	for cur.Next(context.Background()) {
+		var result bson.M
+		e := cur.Decode(&result)
+		if e != nil {
+			utils.GetError(err, w)
+			return
+		}
+		results = append(results, result)
+
+	}
+
+	if err := cur.Err(); err != nil {
+		utils.GetError(err, w)
+		return
+	}
+
+	cur.Close(context.Background())
+	payload := results
+	json.NewEncoder(w).Encode(payload)
 
 }
