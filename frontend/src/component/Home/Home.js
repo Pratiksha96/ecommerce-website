f0ea7611071316036817629e9704d1cd68b3624f
Aashish Dhalla <aashish.d.220@gmail.com>
2022-02-04 20:41:22 -0500

    Connectivity Check

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -2,8 +2,16 @@ import React, { Fragment } from 'react'
 import Product from './Product.js'
 import './Home.css'
 import Metadata from '../layout/MetaData'
+import axios from 'axios';
+
 const Home = () => {
 
+    async function getData(){
+        const data=await axios.get('http://localhost:8081/product/get')
+        console.log(data)
+    }
+    getData();
+    
     const product ={
         name:'Blue Tshirt',
         images:[{url:"https://i.ibb.co/DRST11n/1.webp"}],
