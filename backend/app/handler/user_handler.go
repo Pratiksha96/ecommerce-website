425082f8fc2b0bab053cd96a1ae7c743459f1b99
Pranav Seth <pranavseth26@gmail.com>
2022-02-27 00:55:29 -0500

    Resolved #68 by adding user logout api

--- backend/app/handler/user_handler.go
+++ backend/app/handler/user_handler.go
@@ -1,13 +1,13 @@
 package handler
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
-
 	models "ecommerce-website/app/Models"
 	"ecommerce-website/app/middleware"
 	"ecommerce-website/app/utils"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
 )
 
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
@@ -48,4 +48,28 @@ func LoginUser(w http.ResponseWriter, r *http.Request) {
 		middleware.LoginUser(user, w)
 	}
 
+}
+func LogoutUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	cookieToken, err := r.Cookie("token")
+
+	if err != nil || len(cookieToken.Value) == 0 {
+		if err == http.ErrNoCookie {
+			// If the cookie is not set, return an unauthorized status
+			w.WriteHeader(http.StatusUnauthorized)
+			utils.GetError(errors.New("received ErrNoCookie, you are already logged out"), w)
+			return
+		}
+		// For any other type of error, return a bad request status
+		w.WriteHeader(http.StatusBadRequest)
+		utils.GetError(errors.New("please login to access this resource"), w)
+		return
+	}
+
+	tokenStr := cookieToken.Value
+	middleware.LogoutUser(tokenStr, w)
 }
