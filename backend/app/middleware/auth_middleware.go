5b9470381ae0c634c3beaf9434f3723773b167eb
Pratiksha96 <jainpratiksha2110@gmail.com>
2022-03-04 04:44:43 -0500

    Added unit test for get and create product APIs

--- backend/app/middleware/auth_middleware.go
+++ backend/app/middleware/auth_middleware.go
@@ -2,14 +2,11 @@ package middleware
 
 import (
 	"context"
-	models "ecommerce-website/app/models"
 	"ecommerce-website/app/utils"
-	"ecommerce-website/internal/database"
 	"errors"
 	"net/http"
 
 	"github.com/dgrijalva/jwt-go"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 func AuthenticateUser(next http.HandlerFunc) http.HandlerFunc {
@@ -60,21 +57,4 @@ func AuthenticateUser(next http.HandlerFunc) http.HandlerFunc {
 		next.ServeHTTP(w, r)
 		return
 	}
-}
-
-func AuthorizeUser(w http.ResponseWriter, role string, email string) bool {
-	var user models.User
-	userFilter := bson.M{"email": email}
-	userErr := database.Coll_user.FindOne(context.TODO(), userFilter).Decode(&user)
-
-	if userErr != nil {
-		utils.GetError(userErr, w)
-		return false
-	}
-
-	if role == "admin" && (role != user.Role) {
-		utils.GetError(errors.New("sorry, you don't have access to this resource"), w)
-		return false
-	}
-	return true
 }
