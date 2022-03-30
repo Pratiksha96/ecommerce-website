00c31639af212b7115d08f2bfc6ac6b977b1816b
Swati Bansal <swatibansal.aiesec@gmail.com>
2022-03-30 17:48:31 -0400

    Resolved #130 by adding order model required for storing order details

---
+++ backend/app/models/order_models.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"time"
+)
+
+type Order struct {
+	ShippingInfo  AddressInfo `json:"shippingInfo" bson:"shippingInfo"`
+	OrderItems    []*Items    `json:"description" bson:"description"`
+	User          User        `json:"user" bson:"user"`
+	PaymentInfo   Payment     `json:"payment" bson:"payment"`
+	PaidAt        time.Time   `json:"paidAt" bson:"paidAt"`
+	ItemsPrice    int         `json:"itemsPrice" bson:"itemsPrice"`
+	TaxPrice      int         `json:"taxPrice" bson:"taxPrice"`
+	ShippingPrice int         `json:"shippingPrice" bson:"shippingPrice"`
+	TotalPrice    int         `json:"totalPrice" bson:"totalPrice"`
+	OrderStatus   string      `json:"orderStatus" bson:"orderStatus"`
+	DeliveredAt   time.Time   `json:"deliveredAt" bson:"deliveredAt"`
+	CreatedAt     time.Time   `json:"createdAt" bson:"createdAt"`
+}
+
+type AddressInfo struct {
+	Address string `json:"address" bson:"address"`
+	City    string `json:"city" bson:"city"`
+	State   string `json:"state" bson:"state"`
+	Country string `json:"country" bson:"country"`
+	PinCode int    `json:"pinCode" bson:"pinCode"`
+	PhoneNo int    `json:"phoneNo" bson:"phoneNo"`
+}
+
+type Items struct {
+	Name     string  `json:"name" bson:"name"`
+	Price    int     `json:"price" bson:"price"`
+	Quantity int     `json:"quantity" bson:"quantity"`
+	Image    string  `json:"image" bson:"image"`
+	Product  Product `json:"product" bson:"product"`
+}
+
+type Payment struct {
+	Id     string `json:"id" bson:"id"`
+	Status string `json:"status" bson:"status"`
+}
