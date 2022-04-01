f33bc255e7e61e2b72971e47da544cd6b69dd7d2
Swati Bansal <swatibansal.aiesec@gmail.com>
2022-04-01 02:31:40 -0400

    Resolved #141 by adding support for updating orders and product stock count

--- backend/app/handler/order_handler.go
+++ backend/app/handler/order_handler.go
@@ -8,10 +8,15 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+type UpdateOrderRequest struct {
+	Status string `json:"status"`
+}
 
 func CreateOrder(orderManager manager.OrderManager) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +37,9 @@ func CreateOrder(orderManager manager.OrderManager) http.HandlerFunc {
 		}
 		ctx := r.Context()
 		email := ctx.Value("email").(string)
-		order.OrderStatus = "processing"
+		order.OrderStatus = "Processing"
+		order.PaidAt = time.Now()
+		order.CreatedAt = time.Now()
 		//adding user id for now
 		order.User = email
 		response, err := orderManager.CreateOrder(order, "user", email)
@@ -117,5 +124,30 @@ func DeleteOrder(orderManager manager.OrderManager) http.HandlerFunc {
 			return
 		}
 		json.NewEncoder(w).Encode(deleteResponse)
+	}
+}
+
+//for admin use only
+func UpdateOrder(orderManager manager.OrderManager) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		var params = mux.Vars(r)
+		id, err := primitive.ObjectIDFromHex(params["id"])
+		if err != nil || params["id"] == "" {
+			utils.GetErrorWithStatus(errors.New("invalid object id"), w, http.StatusUnprocessableEntity)
+			return
+		}
+		ctx := r.Context()
+		email := ctx.Value("email").(string)
+		var status UpdateOrderRequest
+		_ = json.NewDecoder(r.Body).Decode(&status)
+
+		updateResponse, err := orderManager.UpdateOrder(status.Status, id, "admin", email)
+		if err != nil {
+			utils.GetError(err, w)
+			return
+		}
+		json.NewEncoder(w).Encode(updateResponse)
 	}
 }
--- backend/app/models/models.go
+++ backend/app/models/models.go
@@ -1,14 +1,19 @@
 package models
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Product struct {
-	Name        string    `json:"name" bson:"name"`
-	Description string    `json:"description" bson:"description"`
-	Price       int       `json:"price" bson:"price"`
-	Ratings     int       `json:"ratings" bson:"ratings"`
-	Images      []*Image  `json:"images" bson:"images"`
-	Category    string    `json:"category" bson:"category"`
-	Stock       int       `json:"stock" bson:"stock"`
-	Reviews     []*Review `json:"reviews" bson:"reviews"`
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Name        string             `json:"name" bson:"name"`
+	Description string             `json:"description" bson:"description"`
+	Price       int                `json:"price" bson:"price"`
+	Ratings     int                `json:"ratings" bson:"ratings"`
+	Images      []*Image           `json:"images" bson:"images"`
+	Category    string             `json:"category" bson:"category"`
+	Stock       int                `json:"stock" bson:"stock"`
+	Reviews     []*Review          `json:"reviews" bson:"reviews"`
 }
 
 type Review struct {
