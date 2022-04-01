bcc417949abe861b89d18031d015ea171f1c5fff
Aashish Dhalla <aashish.d.220@gmail.com>
2022-03-31 21:25:54 -0400

    user menu added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './component/layout/Header/Header.js'
 import Footer from './component/layout/Footer/Footer.js'
 import Loader from './component/layout/Loader/Loader.js'
-
+import { useSelector } from "react-redux";
 import Home from './component/Home/Home.js'
 import Contact from './component/Contact/Contact.js';
 import About from './component/About/About.js'
@@ -13,19 +13,26 @@ import WebFont from 'webfontloader';
 import Products from './component/Product/Products.js'
 import Search from './component/Product/Search.js'
 import LoginSignUp from './component/User/LoginSignUp.js'
-
-
+import {loadUser} from './actions/userAction.js'
+import store from './store'
+import UserOptions from "./component/layout/Header/UserOptions";
 const App = () => {
+
+  const { isAuthenticated, user } = useSelector((state) => state.user);
+
   React.useEffect(()=>{
     WebFont.load({
       google:{
         families:["Papyrus", "Droid Sans","Chilanka"]
       }
     })
+  store.dispatch(loadUser()  )
     },[]);
   return (
     <Router>
         <Header/> 
+        {/* {isAuthenticated && <UserOptions user={user} />} */}
+        <UserOptions user={user} />
         <Route exact path="/" component={Home}/>
         <Route exact path="/contact" component={Contact}/>
         <Route exact path="/products" component={Products}/>
